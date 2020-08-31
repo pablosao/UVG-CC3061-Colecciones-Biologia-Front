2d52db9811ba2d9857356e73d4822ef702064f7c
Majo <cas181202@uvg.edu.gt>
2020-08-31 00:14:54 -0600

    Adding the search section
    Agregando librerias que nos van a servira
    
    Co-Authored-By: eli17048 <42878138+eli17048@users.noreply.github.com>

--- colecciones-biologia/src/components/App/App.js
+++ colecciones-biologia/src/components/App/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import styles from './App.css';
+import Buscar from '../Buscar';
 
 function App() {
-  return <div className="App">Proyecto Biologia</div>;
+  return (
+  <div className="App">
+    <div className={`container-fluid px-0 ${styles.app}`}>
+        <Buscar />
+      </div>
+  </div>)
 }
 
 export default App;
