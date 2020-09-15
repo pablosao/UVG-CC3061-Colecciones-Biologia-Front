1b786203bf0c82e5fc9de746a54b3ab348a25f1b
eli17048 <andrea.elias16@gmail.com>
2020-09-14 20:02:30 -0600

    search suite done

--- colecciones-biologia/src/tests/spec.test.js
+++ colecciones-biologia/src/tests/spec.test.js
@@ -2,6 +2,7 @@ import reducer from '../reducers/navbar'
 import actions from '../actions/navbar'
 import * as types from '../actions/navbar'
 
+
 describe('navbar reducers', () => {
     it('should return click state', () => {
         expect(reducer(undefined, false)).toEqual(
@@ -14,19 +15,14 @@ describe('navbar reducers', () => {
     })
 
 })
-
-describe('navbar actions', () => {
+describe('navbar actions', () =>{
     it('should return click state', () => {
-        const showHideMenu = types.HAMBURGUER_CLICKED
+        const showHideMenu = types.HAMBURGER_CLICKED
         const expectedAction = {
             type: types.HAMBURGER_CLICKED,
             showHideMenu
+
         }
-
+    })
 })
-})
-
-
-
-
 
