42ad84621c0532e1cc74b068010e896270893f46
juanferdeleon <dele17822@uvg.edu.gt>
2020-12-01 00:41:49 -0600

    Reslutados

--- colecciones-biologia/src/components/Resultados/results.js
+++ colecciones-biologia/src/components/Resultados/results.js
@@ -6,46 +6,51 @@ import * as selectors from "../../reducers";
 
 //import "./style.css";
 
-const Results = ({ searching }) => {
-  //const { resultados } = this.props; 
-  console.log(searching)
+const Results = ({ searching, isAuthenticated }) => {
+  //const { resultados } = this.props;
+  console.log(searching);
   //const {id} = useParams()
-  
-  return (
-    <div>
-      <div className="row">
-      <div className ="col-lg-1"></div>
-      <div className ="col col-lg-10 col-sm-12">
-        <br></br>
-        <h3>Resultados de Busqueda</h3>
-        <hr></hr>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Nombre Científico</th>
-            <th scope="col">Nombre Común</th>
-            <th scope="col">Departamento</th>
-            <th scope="col">Opciones</th>
-          </tr>
-          </thead>
-          <tbody>
-            {searching.map((data, idx) => (
-              <tr>
-                <th scope="row">
-                  {data.scientific_name}</th>
-                <td>{data.common_name}</td>
-                <td>{data.id}</td>
-                <td>{data.location}</td>
-              </tr>))}
-          <br></br>
-        </tbody>
-      </table>
+
+  if (searching) {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-lg-1"></div>
+          <div className="col col-lg-10 col-sm-12">
+            <br></br>
+            <h3>Resultados de Busqueda</h3>
+            <hr></hr>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Nombre Científico</th>
+                  <th scope="col">Nombre Común</th>
+                  <th scope="col">Departamento</th>
+                  {isAuthenticated ? <th scope="col">Opciones</th> : null}
+                </tr>
+              </thead>
+              <tbody>
+                {searching.map((data, idx) => (
+                  <tr>
+                    <th scope="row">{data.scientific_name}</th>
+                    <td>{data.common_name}</td>
+                    <td>{data.departamento.description}</td>
+                    {isAuthenticated ? <td>{data.location}</td> : null}
+                  </tr>
+                ))}
+                <br></br>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-  );
+    );
+  }
+
+  return <div>Hola</div>;
 };
 
 export default connect((state) => ({
   searching: selectors.getSearchResults(state),
+  isAuthenticated: selectors.isAuthenticated(state),
 }))(Results);
